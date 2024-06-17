5d49dd9d0f4f6d40cb39d45c30d29492ba22728c
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-06-17 08:04:33 +0100

    Update rostring.c

--- Level_4/rostring.c
+++ Level_4/rostring.c
@@ -1,82 +1,38 @@
-/*
-Assignment name  : rostring
-Expected files   : rostring.c
-Allowed functions: write, malloc, free
---------------------------------------------------------------------------------
-
-Write a program that takes a string and displays this string after rotating it
-one word to the left.
-
-Thus, the first word becomes the last, and others stay in the same order.
-
-A "word" is defined as a part of a string delimited either by spaces/tabs, or
-by the start/end of the string.
-
-Words will be separated by only one space in the output.
-
-If there's less than one argument, the program displays \n.
-
-Example:
-
-$>./rostring "abc   " | cat -e
-abc$
-$>
-$>./rostring "Que la      lumiere soit et la lumiere fut"
-la lumiere soit et la lumiere fut Que
-$>
-$>./rostring "     AkjhZ zLKIJz , 23y"
-zLKIJz , 23y AkjhZ
-$>
-$>./rostring "first" "2" "11000000"
-first
-$>
-$>./rostring | cat -e
-$
-$>
-*/
-
-//escrever um programa onde a primeira palavra vai ser a ultima
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int ft_isspace(char c)
+int ft_is_space(char c)
 {
 	return ((c == ' ') || (c == '\t'));
 }
 
-int main (int ac, char **av)
+int		main(int ac, char **av)
 {
-	char *str;
-	int word_start;
-	int word_end;
-	int space_found;
-	int c;
+	int		i;
+	int		word_start;
 
-	if (ac >= 2)
+	if (ac > 1)
 	{
-		str = av[1];
-		c = 0;
-		while (ft_isspace(str[c]))
-			c++;
-		word_start = c;
-		while (str[c] && !ft_isspace(str[c]))
-			c++;
-		word_end = c;
-		while (ft_isspace(str[c]))
-			c++;
-		while (str[c])
+		i = 0;
+		while (ft_is_space(av[1][i]))
+			i++;
+		word_start = i;
+		while (av[1][i] && !ft_is_space(av[1][i]))
+			i++;
+		while (av[1][i])
 		{
-			while (ft_isspace(str[c]) && ft_isspace(str[c + 1]))
-				c++;
-			if (ft_isspace(str[c]))
-				space_found = 1;
-			write (1, &str[c++], 1);
+			if (av[1][i] && !ft_is_space(av[1][i]) && ft_is_space(av[1][i - 1]))
+			{
+				while (av[1][i] && !ft_is_space(av[1][i]))
+					write(1, &av[1][i++], 1);
+				write(1, " ", 1);
+			}
+			i++;
 		}
-		if (space_found)
-			write (1, " ", 1);
-		while (word_start < word_end)
-			write (1, &str[word_start++], 1);
+		while (av[1][word_start] && !ft_is_space(av[1][word_start]))
+			write(1, &av[1][word_start++], 1);
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
 }
-
