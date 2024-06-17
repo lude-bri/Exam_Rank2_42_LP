32b2630306ec3814a50440845862e4babcb6c682
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-06-17 10:05:43 +0100

    Update rev_wstr.c

--- Level_4/rev_wstr.c
+++ Level_4/rev_wstr.c
@@ -30,29 +30,11 @@ $>
 */
 
 #include <unistd.h>
-
-/*	Print words in string in reverse order
- *
- *	Get the 'len' of input 'str';
- *	Loop through 'str' from the end;
- *		Skip spaces and the NULL character;
- *		Save 'word_end';
- *		Skip non-space characters;
- *		Save 'word_start';
- *		Set 'str_idx' to 'word_start';
- *		Print 'word_start' to 'word_end';
- *		If str_idx is not 0
- *			Print ' '
- *	Print '\n'
- *	Return
- *
- */
+#include <stdlib.h>
 
 int	ft_isspace(char c)
 {
-	if ((c == ' ') || (c == '\t'))
-		return (1);
-	return (0);
+	return ((c == ' ') || (c == '-'));
 }
 
 int ft_strlen(char *str)
@@ -63,30 +45,25 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	char	*str;
-	int		word_start;
-	int		word_end;
-	int		len;
-	int		str_idx;
+	int word_start, word_end, len, i;
 
-	if (argc == 2)
+	if (ac == 2)
 	{
-		str = argv[1];
-		len = ft_strlen(str);
+		len = ft_strlen(av[1]);
 		while (len >= 0)
 		{
-			while (!str[len] || ft_isspace(str[len]))
+			while (ft_isspace(av[1][len]) || !av[1][len])
 				--len;
 			word_end = len;
-			while (str[len] && !ft_isspace(str[len]))
+			while (!ft_isspace(av[1][len]) && av[1][len])
 				--len;
 			word_start = (len + 1);
-			str_idx = word_start;
+			i = word_start;
 			while (word_start <= word_end)
-				write(1, &str[word_start++], 1);
-			if (str_idx != 0)
+				write(1, &av[1][word_start++], 1);
+			if (i != 0)
 				write(1, " ", 1);
 		}
 	}
